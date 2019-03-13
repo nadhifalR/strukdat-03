46b659aeedad3b14c4b0eadeee229f8fafde46fd
NadhifalR <nadhifal01@gmail.com>
2019-03-13 11:31:36 +0700

    strukdat-03

---
+++ exercise-01.cpp
@@ -0,0 +1,46 @@
+/*
+ * Author : Nadhifal
+ * NPM : 140810180048
+ * Kelas : B
+ * Deskripsi :
+ * Tanggal : 13 Maret 2019
+ */
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+typedef struct {
+ float panjang;
+ float lebar;
+} segiempat;
+
+void input(segiempat *s){
+    cout<<"Panjang : ";cin>> s->panjang;
+    cout<<"Lebar   : ";cin>> s->lebar;
+}
+
+float keliling(segiempat s){
+    return 2* (s.panjang + s.lebar);
+}
+
+float luas(segiempat s){
+    return s.panjang*s.lebar;
+}
+
+float diagonal(segiempat s){
+    return sqrt((s.panjang*s.panjang) + (s.lebar*s.lebar));
+}
+
+void print(segiempat s){
+    cout<<"Keliling : "<<keliling(s)<<endl;
+    cout<<"Luas : "<<luas(s)<<endl;
+    cout<<"Diagonal : "<<diagonal(s)<<endl;
+}
+
+int main() {
+   segiempat* sg;
+   sg = new segiempat;
+   input(sg);
+   print(*sg);
+}
